86808289ec7c55433ff32819a8f030d2d09477a7
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-05-14 17:09:23 -0400

    Updated

--- src/DogPics.js
+++ src/DogPics.js
@@ -6,14 +6,22 @@ function DogPics() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
+        // setting state in the useEffect callback
         setImages(data.message);
       });
   });
+
+  return (
+    <div>
+      {images.map((image) => (
+        <img src={image} key={image} />
+      ))}
+    </div>
+  );
+}
 
   console.log("render");
 
